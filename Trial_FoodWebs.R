03016e07cf5ed140c56c2386f294c0ecb7454a0d
Sophie Donnet <sophie.donnet@inrae.fr>
2023-06-23 18:04:17 +0200

    **

--- Trial_FoodWebs.R
+++ Trial_FoodWebs.R
@@ -5,18 +5,15 @@ if(Sys.info()[[4]]=='sophie-Latitude-5310'){
 if(Sys.info()[[4]]=="donnet-Precision-Tower-5810"){
   setwd('/home/donnet/WORK_ALL/RECHERCHE/TRAVAUX_RECHERCHE/Barbillon-Chabert/BayesianColSBM')
 }
-#--------------------------------- 
+############################ Files and data
 library(sbm)
 library(gtools)
 library(DescTools)
 sapply(list.files(paste0(getwd(),'/Functions'),full.names = TRUE), source)
 
-
-#------------------ 
-pathtodata <- paste0(getwd(),'/dataThomson')
+#------------------------- all data
+pathtodata <- paste0(getwd(),'/dataThomson/data')
 my_files <- list.files(pathtodata)
-
-
 food_webs <- lapply(
   X = seq_along(my_files),
   FUN = function(i) {
@@ -35,93 +32,119 @@ site_names <- c("Martins(M)", "Cooper(NC)", "Herlzier(NC)", "Venlaw(NC)",
                 "Berwick(NZ)", "NorthCol(NZ)", "Powder(NZ)", "Trib(NZ)" )
 names(food_webs)<- site_names
 
-#for (i in 1:length(site_names)){
+#########################################################"
+#---------------------- What to do 
+#########################################################"
 
-i = 1 
-name_i <- site_names[i]
-Li <- food_webs[[i]]
-Mat <- Li$net
-nr <- Li$nr
-U <- order(rownames(Mat))
-Mat <- Mat[U,U]
+net <- 'simple' # collec
+estim <- FALSE # 'No' # "Simple",'iidColSbm'
+#------------------ 
+
+if(net=='simple'){
+  pathtores <- paste0(getwd(),'/dataThomson/res_estim_simple')
+  extr = 1 
+}else{
+  pathtores <- paste0(getwd(),'/dataThomson/res_estim_iidColSbm')
+  extr = 1:3
+}
+#---------------- collec networks
+collecNetworks  = list()
+nbNodes <- rep(0,length(extr))
+for (i in extr){
+  name_i <- site_names[i]
+  Li <- food_webs[[i]]
+  Mat <- Li$net
+  nr <- Li$nr
+  U <- order(rownames(Mat))
+  Mat <- Mat[U,U]
+  collecNetworks[[i]] <- Mat
+  nbNodes[i]= nr
+}
+names(collecNetworks) <- site_names[extr]
+mydata <- list(collecNetworks  = collecNetworks, M = length(extr), nbNodes = nbNodes)
 
 #################### 
 ### Model 
 ########################
 
 emissionDist = 'bernoulli'
-model = 'piColSBM'
-collecNetworks <- list(Mat)
-M = 1
-mydata <- list(collecNetworks  = collecNetworks, M= 1, nbNodes = nr)
+model = 'iidColSBM'
 
-###########################################################################################
-#------------------ VBEM 
-###########################################################################################
-#--------------- init CollecTau
-initSimple <- lapply(collecNetworks,estimateSimpleSBM)
-myRef = which.max(sum(t(sapply(initSimple,function(m){m$nbBlocks}))))
-collecTau_init <- initCollecTau(initSimple, ref = myRef)
-KEstim <- initSimple[[myRef]]$nbBlocks[1]
-print(c(KEstim))
-
-#---------------  Prior distribution
-
-hyperparamPrior <- setHyperparamPrior(M,KEstim, emissionDist, model)
 
 
 
 ############################################################
 ######################## VB ###############################
 #############################################################
-#------------- variational estim 
+if(net == 'simple'){
+  names_files_res <- paste0(pathtores,'/res_estim_',site_names[i],'_VB.Rdata')
+}
 
-estimOptionsVBEM <- list(maxIterVB = 1000,
+
+if(estim){
+  ###########################################################################################
+  #------------------ VBEM 
+  ###########################################################################################
+  #--------------- init CollecTau
+  initSimple <- lapply(collecNetworks,estimateSimpleSBM)
+  myRef = which.max(sum(t(sapply(initSimple,function(m){m$nbBlocks}))))
+  collecTau_init <- initCollecTau(initSimple, ref = myRef)
+  KEstim <- initSimple[[myRef]]$nbBlocks[1]
+  print(c(KEstim))
+  
+  #---------------  Prior distribution
+  
+  hyperparamPrior <- setHyperparamPrior(M,KEstim, emissionDist, model)
+  
+  
+  estimOptionsVBEM <- list(maxIterVB = 1000,
                          maxIterVE = 100,
                          valStopCritVE = 10^-10,
                          valStopCritVB = 10^-10,
                          epsTau = 10^-3)
-nbNodes <-  t(sapply(initSimple, function(sbm){sbm$nbNodes}))
-resEstimVBEM  <- VBEMColSBM(mydata,hyperparamPrior,collecTau_init,estimOptions = estimOptionsVBEM, emissionDist, model)
+  nbNodes <-  t(sapply(initSimple, function(sbm){sbm$nbNodes}))
 
-#------------------ Set ApproxPost
+  resEstimVBEM  <- VBEMColSBM(mydata,hyperparamPrior,collecTau_init,estimOptions = estimOptionsVBEM, emissionDist, model)
 
-hyperparamApproxPost <- resEstimVBEM$hyperparamPost
-hyperparamApproxPost$collecTau <- resEstimVBEM$collecTau
+  #------------------ Set ApproxPost
 
-names_files_res <- paste0(pathtodata,'/res_estim_',name_i,'_VB.Rdata')
-save(hyperparamPrior,collecTau_init,hyperparamApproxPost,names_files_res,file = names_files_res )
+  hyperparamApproxPost <- resEstimVBEM$hyperparamPost
+  hyperparamApproxPost$collecTau <- resEstimVBEM$collecTau
 
+  save(hyperparamPrior,collecTau_init,hyperparamApproxPost,names_files_res,file = names_files_res )
+}else{
+  load(file = names_files_res)
+}
 
 
 
 ###########################################################################################
 #------------------  SMC 
 ###########################################################################################
-estimOptionsSMC = list()
-estimOptionsSMC$paramsMCMC <- list(nbIterMCMC=5)  
-estimOptionsSMC$MC <- 2000
-estimOptionsSMC$ESS.rate <- 0.9
-estimOptionsSMC$cESS.rate <- 0.9
-estimOptionsSMC$opSave <- FALSE
-estimOptionsSMC$op.parallel  <- list(os =  .Platform$OS.type, mc.cores = 4); 
-estimOptionsSMC$op.print<- TRUE
-estimOptionsSMC$NB.iter.max  <- Inf # Inf
-estimOptionsSMC$op.SMC.classic <- FALSE
-resSMC <- SMCColSBM(data = mydata,hyperparamPrior,hyperparamApproxPost, emissionDist, model, estimOptionsSMC)
 
-names_files_res <- paste0(pathtodata,'/res_estim_',name_i,'_SMCVB.Rdata')
-save(estimOptionsSMC, hyperparamPrior,hyperparamApproxPost,resSMC,names_files_res,file = names_files_res )
 
+if(net == 'simple'){
+  names_files_res <- paste0(pathtores,'/res_estim_',site_names[i],'_SMCVB.Rdata')
+}else{
+  names_files_res <- paste0(pathtores,'/res_estim_SMCVB.Rdata')
+}
+if(estim){
+  estimOptionsSMC = list()
+  estimOptionsSMC$paramsMCMC <- list(nbIterMCMC=5)  
+  estimOptionsSMC$MC <- 1000
+  estimOptionsSMC$ESS.rate <- 0.9
+  estimOptionsSMC$cESS.rate <- 0.9
+  estimOptionsSMC$opSave <- FALSE
+  estimOptionsSMC$op.parallel  <- list(os =  .Platform$OS.type, mc.cores = 4); 
+  estimOptionsSMC$op.print<- TRUE
+  estimOptionsSMC$NB.iter.max  <- Inf # Inf
+  estimOptionsSMC$op.SMC.classic <- FALSE
+  resSMC <- SMCColSBM(data = mydata,hyperparamPrior,hyperparamApproxPost, emissionDist, model, estimOptionsSMC)
+  save(estimOptionsSMC, hyperparamPrior,hyperparamApproxPost,resSMC,names_files_res,file = names_files_res )
+}else{
+  load(file=names_files_res)
+}
 plot(resSMC$alpha.vec,type='l')
-
-rm(hyperparamApproxPost)
-estimOptionsSMC$op.SMC.classic <- TRUE
-resSMC_classic <- SMCColSBM(data = mydata,hyperparamPrior,hyperparamApproxPost = NULL, emissionDist, model, estimOptionsSMC)
-names_files_res <- paste0(pathtodata,'/res_estim_',name_i,'_SMCClassic.Rdata')
-save(estimOptionsSMC, hyperparamPrior,resSMC_classic,names_files_res,file = names_files_res )
-
-
 
 ######################################
 
@@ -130,43 +153,65 @@ save(estimOptionsSMC, hyperparamPrior,resSMC_classic,names_files_res,file = name
 #####################################
 ##################  Proba d'être dans le même clusterpar VB
 ############################################
-load( paste0(pathtodata,'/res_estim_',name_i,'_VB.Rdata'))
-for (m in 1:M){
-  plotMyMatrix(hyperparamApproxPost$collecTau[[m]] %*%t( hyperparamApproxPost$collecTau[[m]]))
-}
+M = 1
 
-extr <- which(apply(resSMC_classic$HSample_end$connectParamSample,c(3),sum)<4)
-length(extr)
 
-ZZ_VB <- hyperparamApproxPost$collecTau[[1]]%*%t(hyperparamApproxPost$collecTau[[1]])
+ZZ_VB <- lapply(1:M,function(m){hyperparamApproxPost$collecTau[[m]]%*%t(hyperparamApproxPost$collecTau[[m]])})
 
 ZSample <- lapply(1:M,function(m){resSMC$HSample_end$ZSample[[m]]})
 postZSample <- transfZsampleIntoMatrix(ZSample)
-ZZ_SMC  = matrix(0,nbNodes[1],nbNodes[1])
-for (i in 1:nbNodes[1]){
-  for (j in 1:nbNodes[1]){
-    ZZ_SMC [i,j] = sum(resSMC$W.end*(postZSample[[1]][i,]== postZSample[[1]][j,])) 
+ZZ_SMC <- lapply(1:M,function(m){
+  MZZ <- matrix(0,nbNodes[m],nbNodes[m])
+  for (i in 1:nbNodes[m]){
+    for (j in 1:nbNodes[m]){
+      MZZ [i,j] = sum(resSMC$W.end*(postZSample[[m]][i,]== postZSample[[m]][j,])) 
+    }
   }
+  return(MZZ)
 }
+)
 
-ZSample <- lapply(1:M,function(m){resSMC_classic$HSample_end$ZSample[[m]][,,extr]})
-postZSample <- transfZsampleIntoMatrix(ZSample)
-ZZ_SMC_classic  = matrix(0,nbNodes[1],nbNodes[1])
-for (i in 1:nbNodes[1]){
-  for (j in 1:nbNodes[1]){
-    ZZ_SMC_classic [i,j] = sum(resSMC_classic$W.end[extr]*(postZSample[[1]][i,]== postZSample[[1]][j,])) 
-  }
+
+
+#ZSample <- lapply(1:M,function(m){resSMC_classic$HSample_end$ZSample[[m]][,,extr]})
+#postZSample <- transfZsampleIntoMatrix(ZSample)
+#ZZ_SMC_classic  = matrix(0,nbNodes[1],nbNodes[1])
+#for (i in 1:nbNodes[1]){
+#  for (j in 1:nbNodes[1]){
+#    ZZ_SMC_classic [i,j] = sum(resSMC_classic$W.end[extr]*(postZSample[[1]][i,]== postZSample[[1]][j,])) 
+#  }
+#}
+
+UVB <- ZZ_VB[[1]][upper.tri(ZZ_VB[[1]])]
+USMC <- ZZ_SMC[[1]][upper.tri(ZZ_SMC[[1]])]
+ZZ_DF <- cbind(UVB,USMC)
+names(ZZ_DF) <- c('VB','VBSMC')
+ggplot(ZZ_DF) + geom_points(aes(x=VB,y=VBSMC))
+par(mfrow=c(1,2))
+for (m in 1:M){
+  plot(ZZ_VB[[m]][w],ZZ_SMC[[m]][w])
+  plot(ZZ_VB[[m]],ZZ_SMC[[m]])
+  
+  abline(a=0,b=1)
+  
+  hist(ZZ_SMC[[m]])
+  hist(ZZ_VB[[m]],add= TRUE,col='blue')
+  
+  
+  mean( abs(ZZ_SMC[[m]]-1/2))
+  mean( abs(ZZ_VB[[m]]-1/2))
+  
+  hist(abs(1/2- ZZ_VB[[m]]),add= TRUE,col='blue')
+  
+  
 }
-
-
+ZZ_all <- lower.tri(ZZ_SMC)*ZZ_SMC + upper.tri(ZZ_VB)*ZZ_VB
+plotMyMatrix(ZZ_all)
 plotMyMatrix(ZZ_VB)
-plotMyMatrix(ZZ_SMC-ZZ_VB,plotOptions = list(legend = TRUE))
-plotMyMatrix(ZZ_SMC-ZZ_SMC_classic,plotOptions = list(legend = TRUE))
-plot(ZZ_SMC,ZZ_SMC_classic); abline(a=0,b = 1)
-plot(ZZ_VB,ZZ_SMC);abline(a=0,b = 1)
+plotMyMatrix(ZZ_SMC)
+
 hist(ZZ_VB)
-hist(ZZ_SMC)
-hist(ZZ_SMC_classic)
+hist(ZZ_SMC,add = TRUE,col="blue")
 ###############################
 K <- KEstim
 par(mfrow=c(1,1))
